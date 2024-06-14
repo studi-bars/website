a61cab412d976827cc051483320b1db9b269494c
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-06-14 13:52:47 +0200

    Improve Admin for Events

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,8 @@
+from time import timezone
+
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from django.utils import timezone
 
 from main.models import Bar, Event
 
@@ -8,6 +12,36 @@ class BarAdmin(admin.ModelAdmin):
     pass
 
 
+class FutureEventsFilter(SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Event Zeit'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'time'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('0', 'Kommende Events'),
+            ('1', 'Alte Events')
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == "0":
+            return queryset.filter(start_date__gte=timezone.now().date())
+        if self.value() == "1":
+            return queryset.filter(start_date__lt=timezone.now().date())
+
+        return queryset
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'start_date', 'bar')
+    ordering = ('start_date',)
+    list_filter = ('bar', FutureEventsFilter,)
