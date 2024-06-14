24499acef331f366a2a830db9b6653b3f8bfa232
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-06-14 13:52:26 +0200

    Use "normal" rest API and not hyperlinked (id's instead of urls)

--- main/serializers.py
+++ main/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from main.models import Bar, Event
 
 
-class BarSerializer(serializers.HyperlinkedModelSerializer):
+class BarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bar
         fields = '__all__'
 
 
-class EventSerializer(serializers.HyperlinkedModelSerializer):
+class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
