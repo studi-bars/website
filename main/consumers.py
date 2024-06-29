76a971254727e44a4b8973f6ca348a5b0b9a5723
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-06-29 15:31:39 +0200

    Fix subscription to specific bar events

--- main/consumers.py
+++ main/consumers.py
@@ -6,7 +6,7 @@ CHANNEL_GROUP_NAME = 'events'
 
 class EventConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.bar_id = self.scope['url_route']['kwargs'].get('bar_id')
+        self.bar_id = int(self.scope['url_route']['kwargs'].get('bar_id', 0))
         await self.channel_layer.group_add(CHANNEL_GROUP_NAME, self.channel_name)
         await self.accept()
 
@@ -14,6 +14,5 @@ class EventConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(CHANNEL_GROUP_NAME, self.channel_name)
 
     async def event_notification(self, event):
-        if not self.bar_id or self.bar_id == event['event']['data']['id']:
+        if not self.bar_id or self.bar_id == event['event']['data']['bar']:
             await self.send(text_data=json.dumps(event))
-
