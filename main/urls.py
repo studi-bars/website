ac396aed67d535b5d05be35dadc945b1b01c83c2
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-08-28 13:15:39 +0200

    test different url regex

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main import views
 
 urlpatterns = [
     path('main', views.main_view, name='main'),
-    re_path(r"^ics/event/.*(?P<event_id>[0-9]+)/$", views.download_event_ics, name='download_event_ics'),
+    re_path(r"^ics/event/(?P<event_id>[0-9]+)/$", views.download_event_ics, name='download_event_ics'),
     re_path(r"^ics/bar/events/.*(?P<bar_id>[0-9]+)/$", views.download_bar_events_ics, name='download_bar_events_ics'),
     path('bar/<int:bar_id>/<name>/', views.bar_view, name='bar_view'),
     path('event/<int:event_id>/<name>/', views.event_view, name='event_view'),
